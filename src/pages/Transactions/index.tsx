30a34059a9fc43b678cb2a5608ac5a363a5f39fd
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-05-11 19:33:15 -0300

    02: formatando valores

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForm } from './components'
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles'
 
@@ -23,10 +24,13 @@ export function Transactions() {
                 <tr key={transaction.id}>
                   <td width="50%">{transaction.description}</td>
                   <td>
-                    <PriceHighlight $variant={transaction.type}>{transaction.price}</PriceHighlight>
+                    <PriceHighlight $variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
